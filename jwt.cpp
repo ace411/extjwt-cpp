937bfd0f94b8231edc724f94356ff46f7d9f38f3
ace411 <lochbm@live.com>
2019-09-22 13:46:52 +0300

    Removed scope resolution on jwt calls

--- jwt.cpp
+++ jwt.cpp
@@ -2,6 +2,7 @@
 #include "jwt.h"
 
 using namespace std;
+using namespace jwt;
 
 template <typename B, typename L, typename S>
 auto signJwt(B obj, L algo, const S &secret) -> S
@@ -11,18 +12,15 @@ auto signJwt(B obj, L algo, const S &secret) -> S
     switch (algo)
     {
     case JWT_ALGO_HS256:
-        token += obj.sign(jwt::algorithm::hs256{secret});
+        token += obj.sign(algorithm::hs256{secret});
         break;
 
     case JWT_ALGO_HS384:
-        token += obj.sign(jwt::algorithm::hs384{secret});
+        token += obj.sign(algorithm::hs384{secret});
         break;
 
     case JWT_ALGO_HS512:
-        token += obj.sign(jwt::algorithm::hs512{secret});
-        break;
-
-    default:
+        token += obj.sign(algorithm::hs512{secret});
         break;
     }
 
@@ -32,7 +30,7 @@ auto signJwt(B obj, L algo, const S &secret) -> S
 template <typename S, typename L, typename I>
 auto jwtEncode(const S &secret, L algo, I claims) -> S
 {
-    auto tokenObj = jwt::create()
+    auto tokenObj = create()
                         .set_type("JWS");
 
     for (auto &iter : claims)
@@ -45,7 +43,7 @@ auto jwtEncode(const S &secret, L algo, I claims) -> S
         tokenObj.set_issuer(claims["iss"]);
     }
 
-    return signJwt<jwt::builder, long, string>(tokenObj,
+    return signJwt<builder, long, string>(tokenObj,
                                                algo,
                                                secret);
 }
@@ -53,26 +51,28 @@ auto jwtEncode(const S &secret, L algo, I claims) -> S
 template <typename S, typename L>
 auto jwtDecode(const S &token, L algo, const S &secret) -> strmap
 {
-    auto decoded = jwt::decode(token), verifier = jwt::verify();
+    auto decoded = decode(token), verifier = verify();
     strmap claims;
 
     switch (algo)
     {
     case JWT_ALGO_HS256:
-        verifier.allow_algorithm(jwt::algorithm::hs256{secret});
+        verifier.allow_algorithm(algorithm::hs256{secret});
         break;
 
     case JWT_ALGO_HS384:
-        verifier.allow_algorithm(jwt::algorithm::hs384{secret});
+        verifier.allow_algorithm(algorithm::hs384{secret});
         break;
 
     case JWT_ALGO_HS512:
-        verifier.allow_algorithm(jwt::algorithm::hs512{secret});
+        verifier.allow_algorithm(algorithm::hs512{secret});
         break;
 
     default:
         break;
     }
+
+    verifier.verify(decoded);
 
     for (auto &iter : decoded.get_payload_claims())
     {
